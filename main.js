7e183f12afd28c6d66a30522b55750fc905644a6
soominnnn <105346651+soominnnn@users.noreply.github.com>
2023-02-28 14:18:08 +0900

    modified

--- main.js
+++ main.js
@@ -77,42 +77,42 @@ function make_rectang1(){
               tagArea_line.appendChild(line123);
             }
           })
+          firebase.database().ref('service/장바구니/102호').on('value',function(getData){
+            var test = getData.val();
+            var count = getData.numChildren();
+          // 서비스 요청 내역_ 룸 넘버
+            const entries = Object.entries(test);
+            const tester = Object.fromEntries(entries);
+            const keytest = Object.values(tester);
+            const valuetest = Object.keys(tester);
+        
+        
+            make_rectang1();
+            let tagArea_line = document.getElementsByClassName("rectang1")[0];
+            let room_namee = document.createElement('div');
+                    room_namee.setAttribute('class','room_number');
+                    tagArea_line.appendChild(room_namee);
+                    room_namee.innerHTML = "102호"
+        
+            for( var i = 0; i < count; i++)
+            {
+              //룸 내 라인 위 요청 내역
+        
+              let tagArea_line = document.getElementsByClassName("rectang1")[0];
+              let order_p = document.createElement('p');
+              order_p.setAttribute('class','amenity');
+              tagArea_line.appendChild(order_p);
+              order_p.innerHTML = valuetest[i] + "&emsp;" + "&emsp;" + keytest[i];
+        
+          // 룸 내 라인 
+              let line123 = document.createElement('div');
+              line123.setAttribute('class', 'line_room');
+              tagArea_line.appendChild(line123);
+            }
+          })
       }
   })
   //102호
-  firebase.database().ref('service/장바구니/102호').on('value',function(getData){
-    var test = getData.val();
-    var count = getData.numChildren();
-  // 서비스 요청 내역_ 룸 넘버
-    const entries = Object.entries(test);
-    const tester = Object.fromEntries(entries);
-    const keytest = Object.values(tester);
-    const valuetest = Object.keys(tester);
-
-
-    make_rectang1();
-    let tagArea_line = document.getElementsByClassName("rectang1")[0];
-    let room_namee = document.createElement('div');
-            room_namee.setAttribute('class','room_number');
-            tagArea_line.appendChild(room_namee);
-            room_namee.innerHTML = "102호"
-
-    for( var i = 0; i < count; i++)
-    {
-      //룸 내 라인 위 요청 내역
-
-      let tagArea_line = document.getElementsByClassName("rectang1")[0];
-      let order_p = document.createElement('p');
-      order_p.setAttribute('class','amenity');
-      tagArea_line.appendChild(order_p);
-      order_p.innerHTML = valuetest[i] + "&emsp;" + "&emsp;" + keytest[i];
-
-  // 룸 내 라인 
-      let line123 = document.createElement('div');
-      line123.setAttribute('class', 'line_room');
-      tagArea_line.appendChild(line123);
-    }
-  })
 
   //룸 상태 변경 버튼 제작
   const button_list = document.getElementById("button_change");
